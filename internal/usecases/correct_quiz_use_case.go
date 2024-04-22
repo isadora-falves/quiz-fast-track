2d62def7405c8db54696ac985ba98a8569279e7d
isadora-falves <isadora.alves1@aluno.ufop.edu.br>
2024-04-22 20:14:53 -0300

    improvements: remove comments

--- internal/usecases/correct_quiz_use_case.go
+++ internal/usecases/correct_quiz_use_case.go
@@ -49,33 +49,8 @@ func (g *correctQuizUseCase) Execute(input input.QuizInput) (*output.QuizOutput,
 			CorrectOption:  correctAlternative.Option,
 		})
 
-
-		// if err != nil {
-		// 	return output.ResultOutput{}, fmt.Errorf("error retrieving answer for question ID %d: %v", answer.QuestionId, err)
-		// }
-
-		// // User should be able to answer all the questions and then post his/hers answers and get back how many correct answers they had, displayed to the user.
-		// if answer.Option == correctAnswer.Option {
-		// 	correctAnswersCount++
-		// } else {
-		// 	errorCount++
-		// }
-
-		// quizTemplates = append(quizTemplates, output.QuizTemplateOutput{
-		// 	QuestionId:     answer.QuestionId,
-		// 	SelectedOption: answer.Option,
-		// 	CorrectOption:  correctAnswer.Option,
-		// })
 	}
-
-	// resume := fmt.Sprintf("You answered %d questions correctly out of %d. You made %d errors.", correctAnswersCount, len(answers.Answers), errorCount)
-
-	// result := output.ResultOutput{
-	// 	Resume:       resume,
-	// 	Hits:         correctAnswersCount,
-	// 	Erros:        errorCount,
-	// 	QuizTemplate: quizTemplates,
-	// }
+	
 	return &output.QuizOutput{
 		Resume:       fmt.Sprintf("You answered %d question correctly out of %d. You made %d error.", rightsCount, len(input.Answers), wrongsCount),
 		RightAnswers: rightsCount,
