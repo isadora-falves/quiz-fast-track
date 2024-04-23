ae78544a16c7f52e09797173424190ad496d1b70
isadora-falves <isadora.alves1@aluno.ufop.edu.br>
2024-04-23 15:10:50 -0300

    fix: fixing test

--- internal/infra/database/memory/repositories/memory_questions_repository_test.go
+++ internal/infra/database/memory/repositories/memory_questions_repository_test.go
@@ -23,35 +23,18 @@ func TestMemoryQuestionsRepositoryGetAll(t *testing.T) {
 }
 
 func TestMemoryQuestionsRepositoryFindQuestionById(t *testing.T) {
+	g := NewGomegaWithT(t)
 	questions := []entities.Question{
 			{Id: 1, Text: "Question 1"},
 			{Id: 2, Text: "Question 2"},
-			{Id: 3, Text: "Question 3"},
 	}
-
 	repo := NewMemoryQuestionsRepository(&questions)
 
-	tests := []struct {
-			name    string
-			id      int
-			want    *entities.Question
-			wantErr error
-	}{
-			{"Existing question", 2, &questions[1], nil},
-			{"Non-existing question", 4, nil, errors.New("question not found")},
-	}
+	// Act
+	got, err := repo.FindQuestionById(3)
 
-	for _, tt := range tests {
-			t.Run(tt.name, func(t *testing.T) {
-					g := NewGomegaWithT(t)
-					got, err := repo.FindQuestionById(tt.id)
-					g.Expect(got).To(Equal(tt.want), "Check fetched question")
-					if tt.wantErr == nil {
-							g.Expect(err).NotTo(HaveOccurred(), "Expected no error")
-					} else {
-							g.Expect(err).To(MatchError(tt.wantErr), "Check error message")
-					}
-			})
-	}
+	// Assert
+	g.Expect(err).To(MatchError(errors.New("question not found")))
+	g.Expect(got).To(BeNil())
 }
 
